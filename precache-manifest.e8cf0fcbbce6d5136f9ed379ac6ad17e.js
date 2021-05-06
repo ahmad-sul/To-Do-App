0a802481af8ecf1dc96496a8504de411e010a7c1
Ahmad <ahhk.48@gmail.com>
2021-05-06 14:39:46 +0200

    Updates

---
+++ precache-manifest.e8cf0fcbbce6d5136f9ed379ac6ad17e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74f4f5a5057edcc89adf54057249f03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "796fa3146c84f76628e6",
+    "url": "./static/css/main.a4dee5e5.chunk.css"
+  },
+  {
+    "revision": "4698fbd0f21220934f7e",
+    "url": "./static/js/2.05d43a0a.chunk.js"
+  },
+  {
+    "revision": "796fa3146c84f76628e6",
+    "url": "./static/js/main.fe2a9c24.chunk.js"
+  },
+  {
+    "revision": "baab4f6995ba25947e6f",
+    "url": "./static/js/runtime-main.f5f70450.js"
+  }
+]);
